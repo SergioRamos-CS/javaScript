2759230c2990836a0084d335e46060be34651633
Sérgio Ramos <srramos123@gmail.com>
2025-07-13 16:46:54 -0300

    Criação do arquivo `ex009.js`

---
+++ aula08/ex009.js
@@ -0,0 +1,7 @@
+var país = 'japão' // Definindo a variável país
+console.log(`Vivendo em ${país}`) // Exibindo o país
+if (país == 'Brasil') { // Verificando se o país é Brasil
+    console.log('Você é brasileiro!') // Mensagem para brasileiros
+} else {
+    console.log('Você é estrangeiro!') // Mensagem para estrangeiros
+} // Fim do bloco condicional
