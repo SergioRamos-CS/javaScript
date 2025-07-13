b84c5bcd75cac75772a57f8449e7cbfe516ad6da
Sérgio Ramos <srramos123@gmail.com>
2025-07-13 16:39:57 -0300

    Criação do arquivo `ex008.js`

---
+++ aula08/ex008.js
@@ -0,0 +1,6 @@
+var velocidade = 59.99  // Definindo a velocidade do carro
+console.log(`A velocidade do seu carro é ${velocidade} km/h`)
+if (velocidade > 60) { // Verificando se a velocidade é maior que 60 km/h
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!') // Exibindo mensagem de multa
+}
+console.log('Dirija sempre usando cinto de segurança!') // Mensagem de segurança para o usuário
